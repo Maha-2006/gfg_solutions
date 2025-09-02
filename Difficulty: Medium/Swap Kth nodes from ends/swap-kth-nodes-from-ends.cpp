6a1cc1bac49490f8a4ee84e50a3c54126ea7f906
Maha-2006 <163001517+Maha-2006@users.noreply.github.com>
2025-09-02 22:09:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Swap Kth nodes from ends/swap-kth-nodes-from-ends.cpp
@@ -0,0 +1,55 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+        // code here
+        Node* swapKth(Node*head, int k){
+    if(!head) return head;
+    
+    int n = 0;
+    Node* temp = head;
+    while(temp){
+        n++;
+        temp = temp->next;
+    }
+    if (k>n) return head;
+    if(2*k-1==n) return head;
+    
+    Node*prevX = nullptr;
+    Node*x = head;
+    for(int i=1;i<k;i++){
+        prevX = x;
+        x = x->next;
+    }
+    Node* prevY = nullptr;
+    Node* y = head;
+    for(int i=1;i<n-k+1;i++){
+        prevY = y;
+        y = y->next;
+    }
+    if(prevX) prevX->next = y;
+    if(prevY) prevY->next = x;
+    
+    Node*tempNext = x->next;
+    x->next = y->next;
+    y->next = tempNext;
+    
+    if(k==1) head = y;
+    if(k==n) head = x;
+    
+    return head;
+
+
+    }
+};
