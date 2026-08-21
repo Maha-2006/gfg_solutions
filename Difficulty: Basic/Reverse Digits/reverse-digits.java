806f94c149411a8fd055a761f2f9bd72c73217a9
Maha-2006 <163001517+Maha-2006@users.noreply.github.com>
2026-08-21 11:29:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse Digits/reverse-digits.java
+++ Difficulty: Basic/Reverse Digits/reverse-digits.java
@@ -1,13 +1,13 @@
 class Solution {
     public int reverseDigits(int n) {
         // Code here
-        int rev = 0;
+        int copy = 0;
         while(n>0){
-            int lastdigit = n%10;
-          rev = rev*10 + lastdigit;
-            n= n/10;
-            
+         int   lastDigit = n%10;
+            copy = copy*10+lastDigit;
+            n=n/10;
         }
-        return rev;
+        return copy;
+        
     }
 }
