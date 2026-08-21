f00fd14ac7fe2fedfa42647a4e9f4265c969dcb1
Maha-2006 <163001517+Maha-2006@users.noreply.github.com>
2026-08-21 09:34:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static boolean areAnagrams(String s1, String s2) {
+        // code here
+        char [] a= s1.toCharArray();
+        char [] b = s2.toCharArray();
+        
+        Arrays.sort(a);
+        Arrays.sort(b);
+        
+        if(Arrays.equals(a,b)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
